0df2d6749ad40bc3078dc125c8b41e9c84e5065a
Tim Thompson <me@tim-thompson.co.uk>
2019-01-30 19:36:43 +0000

    better image handling

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import urlsplit, urlunsplit
 
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, redirect, url_for
@@ -47,7 +48,6 @@ def article(index):
     content = html.select("div.story-body__inner")
     for div in content:
         for item in div.children:
-            print(item.name)
             if item.name == "p":
                 if item.has_attr("class") and item["class"] == [
                     "story-body__introduction"
@@ -55,7 +55,37 @@ def article(index):
                     article += item.prettify()
                 elif not item.has_attr("class"):
                     article += item.prettify()
+            elif item.name == "h2":
+                article += item.prettify()
             elif item.name == "figure":
+                caption = ""
+                for figcaption in item.select("figcaption"):
+                    for span in figcaption.select("span.media-caption__text"):
+                        caption = span.text
+                for span in item.select("span"):
+                    for div in span.select("div.js-delayed-image-load"):
+                        img_url = urlsplit(div['data-src'])
+                        img_path = img_url.path.split('/')
+                        img_path[2] = str(800)
+                        new_path = ("/".join(img_path))
+                        new_img_url = urlunsplit((
+                            img_url.scheme,
+                            img_url.netloc,
+                            new_path,
+                            img_url.query,
+                            img_url.fragment
+                        ))
+                        article += f"""
+                            <figure class="box">
+                                <img 
+                                    src={new_img_url} 
+                                    alt="{div['data-alt']}"
+                                />
+                                <figcaption>
+                                    <strong>{caption}</strong>
+                                </figcaption>
+                            </figure>
+                        """
                 for img in item.select("img"):
                     caption = item.select("span.media-caption__text")
                     caption = caption[0] if len(caption) > 0 else ""
@@ -63,12 +93,10 @@ def article(index):
                         <figure class="box">
                             {img.prettify()}
                             <figcaption>
-                                {caption}
+                                <strong>{caption}</strong>
                             </figcaption>
                         </figure>
                     """
-                print(item)
-
     title = ""
     for h1 in html.select("h1"):
         title = h1.text
@@ -79,7 +107,3 @@ def article(index):
         headline=headlines[index],
         title=title,
     )
-
-
-if __name__ == "__main__":
-    app.run()
